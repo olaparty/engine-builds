4d240d222a8ae21eddcdc2f42297feb49470ff3f
zhilei <chen.zhilei@olaola.chat>
2022-02-19 09:20:27 +0800

    update

--- build.sh
+++ build.sh
@@ -17,26 +17,28 @@ mkdir -p "${OUT_PATH}/gen"
 export SRC_OUT_PATH="${ENGINE_PATH}/src/out/${TARGET}"
 
 ls -al $SRC_OUT_PATH
-ls -al $SRC_OUT_PATH/obj
-ls -al $SRC_OUT_PATH/gen
-ls -al $SRC_OUT_PATH/clang_x64
+ls -al $SRC_OUT_PATH/obj/flutter
+ls -al $SRC_OUT_PATH/gen/flutter
+ls -al $SRC_OUT_PATH/clang_x64/gen
 
 echo Copying other outputs
 
-cp $SRC_OUT_PATH/libflutter.so $OUT_PATH/libflutter.so ||
-cp $SRC_OUT_PATH/libflutter.dylib $OUT_PATH/libflutter.dylib ||
-(
-  cp $SRC_OUT_PATH/flutter_engine.dll $OUT_PATH/flutter_engine.dll &&
-  cp $SRC_OUT_PATH/flutter_engine.dll.lib $OUT_PATH/flutter_engine.lib &&
-  cp $SRC_OUT_PATH/flutter_engine.dll.exp $OUT_PATH/flutter_engine.exp &&
-  cp $SRC_OUT_PATH/flutter_engine.dll.pdb $OUT_PATH/flutter_engine.pdb
-)
-cp $SRC_OUT_PATH/flutter_embedder.h $OUT_PATH/flutter_embedder.h
-cp $ENGINE_PATH/src/third_party/icu/flutter/icudtl.dat $OUT_PATH/icudtl.dat
-cp -r $SRC_OUT_PATH/flutter_patched_sdk $OUT_PATH/flutter_patched_sdk
-cp $SRC_OUT_PATH/dart $OUT_PATH/dart || true
-cp $SRC_OUT_PATH/gen/frontend_server.dart.snapshot $OUT_PATH/gen/frontend_server.dart.snapshot || true
-cp $SRC_OUT_PATH/gen_snapshot $OUT_PATH/gen_snapshot || true
-cp $SRC_OUT_PATH/clang_x64/gen_snapshot $OUT_PATH/gen_snapshot_x64 || true
-cp $SRC_OUT_PATH/clang_x86/gen_snapshot $OUT_PATH/gen_snapshot_x86 || true
-cp $SRC_OUT_PATH/clang_x64/gen_snapshot_host_targeting_host $OUT_PATH/gen_snapshot_host_targeting_host || true
+cp -R $SRC_OUT_PATH $OUT_PATH
+
+# cp $SRC_OUT_PATH/libflutter.so $OUT_PATH/libflutter.so ||
+# cp $SRC_OUT_PATH/libflutter.dylib $OUT_PATH/libflutter.dylib ||
+# (
+#   cp $SRC_OUT_PATH/flutter_engine.dll $OUT_PATH/flutter_engine.dll &&
+#   cp $SRC_OUT_PATH/flutter_engine.dll.lib $OUT_PATH/flutter_engine.lib &&
+#   cp $SRC_OUT_PATH/flutter_engine.dll.exp $OUT_PATH/flutter_engine.exp &&
+#   cp $SRC_OUT_PATH/flutter_engine.dll.pdb $OUT_PATH/flutter_engine.pdb
+# )
+# cp $SRC_OUT_PATH/flutter_embedder.h $OUT_PATH/flutter_embedder.h
+# cp $ENGINE_PATH/src/third_party/icu/flutter/icudtl.dat $OUT_PATH/icudtl.dat
+# cp -r $SRC_OUT_PATH/flutter_patched_sdk $OUT_PATH/flutter_patched_sdk
+# cp $SRC_OUT_PATH/dart $OUT_PATH/dart || true
+# cp $SRC_OUT_PATH/gen/frontend_server.dart.snapshot $OUT_PATH/gen/frontend_server.dart.snapshot || true
+# cp $SRC_OUT_PATH/gen_snapshot $OUT_PATH/gen_snapshot || true
+# cp $SRC_OUT_PATH/clang_x64/gen_snapshot $OUT_PATH/gen_snapshot_x64 || true
+# cp $SRC_OUT_PATH/clang_x86/gen_snapshot $OUT_PATH/gen_snapshot_x86 || true
+# cp $SRC_OUT_PATH/clang_x64/gen_snapshot_host_targeting_host $OUT_PATH/gen_snapshot_host_targeting_host || true

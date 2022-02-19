e2bb04076fc4fc94e89163ec02acf2a070c9d9a6
Zhilei <84297240+zhilei-ola@users.noreply.github.com>
2022-02-19 11:57:40 +0800

    remove engine patch

--- patch-engine.sh
+++ patch-engine.sh
@@ -1,7 +1,7 @@
 set -e
 # curl -O https://raw.githubusercontent.com/graphitemaster/incbin/main/incbin.h
 # echo -e "\n" >> incbin.h
-cp main.gn engine/src/flutter/BUILD.gn
+# cp main.gn engine/src/flutter/BUILD.gn
 # cp incbin.h engine/src/flutter/shell/platform/embedder/
 # echo "
 # #define INCBIN_SILENCE_BITCODE_WARNING

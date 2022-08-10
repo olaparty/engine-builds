c260ad793ccc480c60db430ebb80a44a5798f307
zhilei <chen.zhilei@olaola.chat>
2022-08-10 23:30:02 +0800

    patch v2.10.3 with support of flutter-assets-dir

--- patch-engine.sh
+++ patch-engine.sh
@@ -9,9 +9,12 @@ set -e
 # INCBIN(Icudtl, \"${PWD}/engine/src/third_party/icu/flutter/icudtl.dat\");
 # " >> engine/src/flutter/shell/platform/embedder/embedder.cc
 
-cp 100522.diff engine/src/flutter/
+cp android_asset_dir.dff engine/src/flutter/
 cd engine/src/flutter 
 
-ls -al shell/gpu
+#ls -al shell/gpu
 
-git apply 100522.diff
+git apply android_asset_dir.diff
+
+git status
+

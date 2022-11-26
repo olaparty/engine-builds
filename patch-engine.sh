7d6ba60079f779639cecb12e577bd5f5eaf0d02e
zhilei <chen.zhilei@olaola.chat>
2022-11-26 11:28:31 +0800

    patch android https://github.com/flutter/engine/pull/32106

--- patch-engine.sh
+++ patch-engine.sh
@@ -9,12 +9,12 @@ set -e
 # INCBIN(Icudtl, \"${PWD}/engine/src/third_party/icu/flutter/icudtl.dat\");
 # " >> engine/src/flutter/shell/platform/embedder/embedder.cc
 
-cp patches/android_asset_dir.diff engine/src/flutter/
+cp patches/32106.diff engine/src/flutter/
 cd engine/src/flutter 
 
 #ls -al shell/gpu
 
-git apply android_asset_dir.diff
+git apply 32106.diff
 
 git status
 

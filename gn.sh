a0ea652ba584d6f92bcfbec7ca248a6ddbcf26db
Zhilei <chen.zhilei@olaola.chat>
2023-02-13 10:46:40 +0800

    build profile engine

--- gn.sh
+++ gn.sh
@@ -2,6 +2,7 @@ set -e
 DEPOT_TOOLS_PATH="${PWD}/depot_tools"
 ENGINE_PATH="${PWD}/engine"
 export PATH="$PATH:$DEPOT_TOOLS_PATH"
+export ANDROID_HOME=$ENGINE_PATH/src/third_party/android_tools/sdk
 
 cd $ENGINE_PATH/src
 ./flutter/tools/gn $@
